c15f09f950b281a61046b5ef2c1b6ed7e11872d9
Drum-J <tmdgh717@naver.com>
2024-01-12 17:01:37 +0900

    섹션 10 : @PostAuthorize를 사용한 메소드 레벨 보안

--- springsecurity_10/src/main/java/com/springsecurity10/controller/LoansController.java
+++ springsecurity_10/src/main/java/com/springsecurity10/controller/LoansController.java
@@ -3,6 +3,7 @@ package com.springsecurity10.controller;
 import com.springsecurity10.model.Loans;
 import com.springsecurity10.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ public class LoansController {
     private final LoanRepository loanRepository;
 
     @GetMapping("/myLoans")
+    @PostAuthorize("hasRole('USER')")
     public List<Loans> getLoanDetails(@RequestParam(name = "id") int id) {
         return loanRepository.findByCustomerIdOrderByStartDtDesc(id);
     }
