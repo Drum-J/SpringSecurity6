e258282be4cef996f4cb4db5166f759ba0bf2c81
Drum-J <tmdgh717@naver.com>
2024-01-12 17:22:39 +0900

    섹션 10 : @PreFilter 애너테이션 실습

--- springsecurity_10/src/main/java/com/springsecurity10/controller/ContactController.java
+++ springsecurity_10/src/main/java/com/springsecurity10/controller/ContactController.java
@@ -3,11 +3,14 @@ package com.springsecurity10.controller;
 import com.springsecurity10.model.Contact;
 import com.springsecurity10.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -20,10 +23,17 @@ public class ContactController {
     private final ContactRepository contactRepository;
 
     @PostMapping("/contact")
-    public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
+    @PreFilter(value = "filterObject.contactName != 'Test'") // Contact.contactName != Test 인 것만 처리하고 싶다.
+    public List<Contact> saveContactInquiryDetails(@RequestBody List<Contact> contacts) {
+        //@PreFilter 를 사용하기 위해서는 반환 타입이 컬렉션이어야 한다.
+        Contact contact = contacts.get(0);
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
-        return contactRepository.save(contact);
+        contact = contactRepository.save(contact);
+
+        List<Contact> responseContact = new ArrayList<>();
+        responseContact.add(contact);
+        return responseContact;
     }
 
     public String getServiceReqNumber() {
